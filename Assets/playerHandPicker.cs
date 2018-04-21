f50053c28535eac9c88a940b4daad9c407424deb
AndrewM <armcmuldroch@gmail.com>
2018-04-21 13:51:17 -0700

    hand can select cards

--- Assets/playerHandPicker.cs
+++ Assets/playerHandPicker.cs
@@ -6,10 +6,15 @@ public class playerHandPicker : MonoBehaviour {
     public GameObject camObject;
     private Camera cam;
     private CardHolder hand;
+    private Card selectify;
+    public GameObject selectShow;
 	// Use this for initialization
 	void Start () {
         cam = camObject.GetComponent<Camera>();
         hand = this.GetComponent<CardHolder>();
+        selectify = null;
+        selectShow = Instantiate(selectShow);
+        selectShow.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -19,8 +24,27 @@ public class playerHandPicker : MonoBehaviour {
         Physics.Raycast(mouseRay, out interfaceHit);
         if (interfaceHit.collider)
         {
-            Debug.DrawLine(new Vector3(0, 0, 0), interfaceHit.transform.position);
-            
+            //Debug.DrawLine(new Vector3(0, 0, 0), interfaceHit.transform.position);
+            Card rayCard = interfaceHit.collider.GetComponent<Card>();
+            BoardPosition bordComp = interfaceHit.collider.GetComponent<BoardPosition>();
+            if (Input.GetMouseButtonDown(0) && rayCard)
+            {
+                selectify = rayCard;
+            }
+            if (selectify)
+            {
+                Debug.DrawLine(selectify.transform.position, interfaceHit.transform.position);
+                selectShow.SetActive(true);
+                selectShow.transform.position = selectify.transform.position;
+                selectShow.transform.rotation = selectify.transform.rotation;
+                selectShow.transform.parent = selectify.transform;
+                selectShow.transform.localPosition -= new Vector3(0,0,.005f);
+            }
+            else
+            {
+                selectShow.SetActive(false);
+            }
         }
+        
     }
 }
