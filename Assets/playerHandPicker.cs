48786928ffe8795437d809106b7dee46459582c7
AndrewM <armcmuldroch@gmail.com>
2018-04-21 14:30:10 -0700

    action line is sick!

--- Assets/playerHandPicker.cs
+++ Assets/playerHandPicker.cs
@@ -8,13 +8,17 @@ public class playerHandPicker : MonoBehaviour {
     private CardHolder hand;
     private Card selectify;
     public GameObject selectShow;
+    private LineRenderer actionLine;
 	// Use this for initialization
 	void Start () {
         cam = camObject.GetComponent<Camera>();
         hand = this.GetComponent<CardHolder>();
         selectify = null;
         selectShow = Instantiate(selectShow);
+        actionLine = selectShow.GetComponent<LineRenderer>();
+        actionLine.positionCount = 20;
         selectShow.SetActive(false);
+        Debug.Log(this.transform.GetComponent<LineRenderer>());
 	}
 	
 	// Update is called once per frame
@@ -31,14 +35,23 @@ public class playerHandPicker : MonoBehaviour {
             {
                 selectify = rayCard;
             }
-            if (selectify)
+            if (selectify!=null)
             {
                 Debug.DrawLine(selectify.transform.position, interfaceHit.transform.position);
                 selectShow.SetActive(true);
                 selectShow.transform.position = selectify.transform.position;
                 selectShow.transform.rotation = selectify.transform.rotation;
-                selectShow.transform.parent = selectify.transform;
-                selectShow.transform.localPosition -= new Vector3(0,0,.005f);
+                //selectShow.transform.parent = selectify.transform;
+                selectShow.transform.position -= selectify.transform.forward*.005f;
+                
+                for (int i = 0; i < actionLine.positionCount; i++)
+                {
+                    float percentLine = i / (actionLine.positionCount - 1f);
+                    float leanBias = selectify.transform.position.x > 0 ? Mathf.Sin(percentLine * Mathf.PI) : -Mathf.Sin(percentLine * Mathf.PI);
+                    Vector3 hoverHeight = new Vector3(leanBias * 2, Mathf.Sin(percentLine * Mathf.PI) * 4, 0);
+                    actionLine.SetPosition(i, selectify.transform.position * (1 - percentLine) + hoverHeight + interfaceHit.transform.position * percentLine);
+                }
+                
             }
             else
             {
